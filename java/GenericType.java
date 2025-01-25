0c03f870f51fa892298b4714f93b5fea7c2df570
AL-FAHAMI <altoihir@gmail.com>
2025-01-26 00:14:52 +0100

    feat: ways of implementing generic type

---
+++ java/GenericType.java
@@ -0,0 +1,29 @@
+public class GenericType {
+
+    // Generic Class
+    public class Box<T> {
+        private T item;
+
+        public T getItem() { return this.item; }
+        public void setItem(T item) { this.item = item; }
+    }
+
+    
+
+    //Main Method
+    public static void main(String[] args) {
+        // We need outer class in order to instantiate a inner class that is not static
+        GenericType outerInstance = new GenericType();
+        // Generic Box that can hold String
+        Box<String> stringBox = outerInstance.new Box<>();
+
+        stringBox.setItem("Hello");
+        System.out.println(stringBox.getItem());
+
+        // Use Integer as type paramaeter
+        Box<Integer> integerBox = outerInstance.new Box<>();
+        integerBox.setItem(4321);
+        System.out.println(integerBox.getItem());
+
+    }
+}
